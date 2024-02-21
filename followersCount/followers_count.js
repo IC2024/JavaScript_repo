48848579503ebcbe1c30fc497977402a3f62963e
IC2024 <ivanhfchan@gmail.com>
2024-02-20 20:01:38 -0500

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -18,3 +18,10 @@ function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
 
+function resetCount() {
+    count =0; // Reset count to -
+    displayCount(); // Display the count
+    //checkCountValue(); // Check count value and display messages
+    alert("Followers count has been reset!");
+}
+
