67b0242885e51b5e3d591c06f041be4d5c5deaa9
IC2024 <ivanhfchan@gmail.com>
2024-02-23 14:57:25 -0500

    add clear all tasks button

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -34,5 +34,11 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
